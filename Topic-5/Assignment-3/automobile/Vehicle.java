ba1f63e8b97210e06a243f36704b8d1ded5fc69e
Abhishek Keshri <keshri.abhishek63@gmail.com>
2021-01-20 03:35:00 +0530

    Add Vehicle.java

---
+++ Topic-5/Assignment-3/automobile/Vehicle.java
@@ -0,0 +1,9 @@
+package automobile;
+
+public abstract class Vehicle {
+    public abstract String getModelName();
+
+    public abstract String getRegistrationNumber();
+
+    public abstract String getOwnerName();
+}
