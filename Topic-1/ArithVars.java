cecf4df2e81164030539a920ae7b927edaa9897b
Abhishek Keshri <keshri.abhishek63@gmail.com>
2021-01-20 00:33:32 +0530

    Add ArithVars.java

---
+++ Topic-1/ArithVars.java
@@ -0,0 +1,10 @@
+public class ArithVars {
+    public static void main(String[] args) {
+        int a = -5 + 8 * 6;
+        int b = 55 + 9 % 9;
+        int c = 20 + -3 * 5 / 8;
+        int d = 5 + 15 / 3 * 2 - 8 % 3;
+
+        System.out.println("a = " + a + ", b = " + b + ", c = " + c + ", d = " + d);
+    }
+}
