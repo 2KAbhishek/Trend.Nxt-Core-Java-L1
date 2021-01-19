461e38030ef252f6d3bce67c5653be8e7ac21839
Abhishek Keshri <keshri.abhishek63@gmail.com>
2021-01-20 00:47:37 +0530

    Add FindMax.java

---
+++ Topic-1/FindMax.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class FindMax {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the size: ");
+        int[] arr = new int[sc.nextInt()];
+        System.out.println("Enter the elements: ");
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+        int max = Integer.MIN_VALUE;
+
+        for (int num : arr) {
+            if (num > max)
+                max = num;
+        }
+        System.out.println("\nMax is " + max);
+    }
+}
