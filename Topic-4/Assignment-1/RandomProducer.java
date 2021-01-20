235881572b128c901af65f19355d5aadb99a59ef
Abhishek Keshri <keshri.abhishek63@gmail.com>
2021-01-20 05:55:23 +0530

    Add RandomProducer.java

---
+++ Topic-4/Assignment-1/RandomProducer.java
@@ -0,0 +1,30 @@
+import java.util.concurrent.BlockingQueue;
+
+public class RandomProducer implements Runnable {
+
+    private BlockingQueue<Integer> queue;
+
+    public RandomProducer(BlockingQueue<Integer> q) {
+        this.queue = q;
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 5; i++) {
+            try {
+                Thread.sleep(i);
+                int random = 1 + (int) (Math.random() * 10);
+                queue.put(random);
+                System.out.println("Number: " + random);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        try {
+            queue.put(0);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
